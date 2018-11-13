2cfd384302feec7ee9a4d5d3c9432bef44078153
rmrotek <remek87@gmail.com>
2018-11-13 18:40:24 +0100

    homeview

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import HomeView from '../HomeView/HomeView';
 
 class App extends Component {
   render() {
     return (
       <>
+        <HomeView />
       </>
     );
   }
---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomeView extends Component {
+  render() {
+    return (
+      <>
+      Home view
+      </>
+    );
+  }
+}
+
+export default HomeView;
