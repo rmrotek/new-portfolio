3e0333ab433ddf8d62d3a575896e14318b80ae86
rmrotek <remek87@gmail.com>
2018-11-14 09:13:35 +0100

    pic import definitely works

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import myFace from '../../myface.jpg'
 
 class HomeView extends Component {
+
+  state= {
+    name: 'Remigiusz',
+    surname: 'Mrotek',
+    picture: myFace,
+
+  }
+
   render() {
     return (
       <>
       Home view
+      <img src={this.state.picture} alt="my face" />;
       </>
     );
   }
