910057210add286297677986ca2c97640b41be15
rmrotek <remek87@gmail.com>
2018-11-14 13:45:08 +0100

    ph project list, need to fit layout, projects in state

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Typography } from '@material-ui/core'
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
-import { IconContext } from "react-icons";
+import { FaGithub, FaLinkedin, FaEnvelope, FaEye } from "react-icons/fa";
+// import { IconContext } from "react-icons";
 
 import Picture from '../Picture/Picture';
 
@@ -11,12 +11,23 @@ class HomeView extends Component {
   state = {
     name: 'Remigiusz',
     surname: 'Mrotek',
+    position: 'Junior Front-end Developer',
     headerDescription: `From the youngest age I was sitting in front of a computer.
     I decided to finally be productive about it and I started to take interest in programming - from July 2018,
     I do it as regularly as possible. I decided to quit my job and sign up for the Front-end daily course to get results as soon as possible.
     Currently, I feel very good (for a 'freshman') in the Front-end environment.
     After learning about React.js I am very reluctant to return to pure HTML and JavaScript.`,
     picture: myFace,
+    userGitHub: 'https://github.com/rmrotek',
+    userLinkedin: 'https://www.linkedin.com/in/remigiusz-mrotek/',
+    userEmail: 'remek87@gmail.com',
+    projects: [{
+      id: 1,
+      title: 'Lazyness Radar',
+      description: 'SPA, React, Firebase - database and auth',
+      homepage: 'https://rmrotek.github.io/lazyness-radar/#/',
+      github: 'https://github.com/rmrotek/lazyness-radar/settings',
+    }],
 
   }
 
@@ -40,6 +51,10 @@ class HomeView extends Component {
                 {`${this.state.name} ${this.state.surname}`}
               </Typography>
 
+              <Typography variant='h5' align='center' paragraph>
+                {this.state.position}
+              </Typography>
+
               <Typography variant='subtitle1' align='left' paragraph>
                 {this.state.headerDescription}
               </Typography>
@@ -54,31 +69,105 @@ class HomeView extends Component {
               <br />
             </Grid>
 
-            <Grid item xl={12} lg={8} md={12} sm={12} xs={11}>
+            <Grid item xl={11} lg={11} md={11} sm={11} xs={11}>
               <Typography variant='h4' align='center' paragraph>
                 Contact information and where to find me
               </Typography>
             </Grid>
 
-            <Grid item container xl={12} lg={8} md={12} sm={12} xs={11} justify='space-around'>
-             {/* temporary inline styles */}
+            <Grid item container xl={11} lg={11} md={11} sm={11} xs={11} justify='space-around'>
+              {/* temporary inline styles */}
 
-                <a href='https://www.linkedin.com/in/remigiusz-mrotek/' target='_blank' rel="noopener noreferrer" style={{color: 'black', fontSize: '3em'}}>
-                  <FaLinkedin />
-                </a>
+              <a href={this.state.userLinkedin} target='_blank' rel="noopener noreferrer" style={{ color: 'black', fontSize: '3em' }}>
+                <FaLinkedin />
+              </a>
 
-                <a href='https://github.com/rmrotek' target='_blank' rel="noopener noreferrer" style={{color: 'black', fontSize: '3em'}}>
-                  <FaGithub />
-                </a>
+              <a href={this.state.userGitHub} target='_blank' rel="noopener noreferrer" style={{ color: 'black', fontSize: '3em' }}>
+                <FaGithub />
+              </a>
 
-                <a href='mailto: remek87@gmail.com' target='_blank' rel="noopener noreferrer" style={{color: 'black', fontSize: '3em'}}>
-                  <FaEnvelope />
-                </a>
-              
+              <a href={`mailto:${this.state.userEmail}`} target='_blank' rel="noopener noreferrer" style={{ color: 'black', fontSize: '3em' }}>
+                <FaEnvelope />
+              </a>
+
             </Grid>
 
             <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
               <br />
+            </Grid>
+
+            <Grid item container xl={11} lg={11} md={11} sm={11} xs={11} justify='center'>
+              <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                <Typography variant='h4' align='center' paragraph>
+                  Projects
+                </Typography>
+              </Grid>
+              {/* proj1 */}
+              <Grid item container xl={12} lg={12} md={12} sm={12} xs={12} justify='flex-start'>
+
+                <Grid item container xl={3} lg={3} md={3} sm={3} xs={12} justify='center' style={{ border: '2px solid black' }}>
+                  <Picture picture={this.state.picture} />
+                </Grid>
+                <Grid item container xl={9} lg={9} md={9} sm={9} xs={12} justify='center' style={{ border: '2px solid black' }}>
+                  {/* <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                    <br />
+                  </Grid> */}
+                  <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                    <Typography variant='h4' align='center' paragraph>
+                      Project title
+                  </Typography>
+                  </Grid>
+                  <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                    <Typography variant='subtitle1' align='center' paragraph>
+                      Project tech used
+                  </Typography>
+                  </Grid>
+                  <Grid item container xl={12} lg={12} md={12} sm={12} xs={12} justify='space-evenly'>
+                    <a href='https://github.com/rmrotek' target='_blank' rel="noopener noreferrer" style={{ color: 'black', fontSize: '3em' }}>
+                      <FaGithub />
+                    </a>
+                    <a href='mailto: remek87@gmail.com' target='_blank' rel="noopener noreferrer" style={{ color: 'black', fontSize: '3em' }}>
+                      <FaEye />
+                    </a>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                <br />
+              </Grid>
+
+              {/* proj2 */}
+              <Grid item container xl={12} lg={12} md={12} sm={12} xs={12} justify='flex-start'>
+                <Grid item container xl={3} lg={3} md={3} sm={3} xs={12} justify='center' style={{ border: '2px solid black' }}>
+                  <Picture picture={this.state.picture} />
+                </Grid>
+                <Grid item container xl={9} lg={9} md={9} sm={9} xs={12} justify='center' style={{ border: '2px solid black' }}>
+                  {/* <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                    <br />
+                  </Grid> */}
+                  <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                    <Typography variant='h4' align='center' paragraph>
+                      Project title
+                  </Typography>
+                  </Grid>
+                  <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+                    <Typography variant='subtitle1' align='center' paragraph>
+                      Project tech used
+                  </Typography>
+                  </Grid>
+                  <Grid item container xl={12} lg={12} md={12} sm={12} xs={12} justify='space-evenly'>
+                    <a href='https://github.com/rmrotek' target='_blank' rel="noopener noreferrer" style={{ color: 'black', fontSize: '3em' }}>
+                      <FaGithub />
+                    </a>
+                    <a href='mailto: remek87@gmail.com' target='_blank' rel="noopener noreferrer" style={{ color: 'black', fontSize: '3em' }}>
+                      <FaEye />
+                    </a>
+                  </Grid>
+                </Grid>
+
+
+              </Grid>
+
             </Grid>
 
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
 import './index.css';
-import 'typeface-roboto';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
